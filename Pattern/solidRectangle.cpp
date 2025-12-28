743f2456eb7c7b2b235397367f8466660d1c3f4b
Ritesh Sharma <123628476+riteshkrm@users.noreply.github.com>
2025-12-28 15:39:51 +0530

    rectangle

---
+++ Pattern/solidRectangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Print Solid Rectangle
+//  * * * * *  --- Row0
+//  * * * * *  --- Row1
+//  * * * * *  --- Row2
+
+
+
+int main(){
+
+    // Outer loop For Row Printing
+    for(int row = 0 ; row < 3; row = row + 1){
+        // Inter loop For Col Printing
+        for(int col = 0 ; col < 5; col = col +1){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
